d8f5ff620e87d5352b122edeaf21acec58c044ed
Aishwary13 <aishwary20490@iiitd.ac.in>
2024-09-24 14:08:31 +0530

    DynamicPathwaySelection working (only sbml graph)

--- SBML_Network/sbml_network.py
+++ SBML_Network/sbml_network.py
@@ -1,134 +1,194 @@
 import xml.etree.ElementTree as ET
+import pandas as pd
 
-# Load and parse the XML file
-tree = ET.parse('fattyacidsynthesis_new_new.xml')
-root = tree.getroot()
 
-# Namespaces used by CellDesigner
 ns = {'celldesigner': 'http://www.sbml.org/2001/ns/celldesigner', '': 'http://www.sbml.org/sbml/level2/version4',
-      'html': 'http://www.w3.org/1999/xhtml'}
+        'html': 'http://www.w3.org/1999/xhtml'}
 
+def parseReaction(rx,idToName):
 
-genes = []
-for gene in root.findall('.//celldesigner:gene', ns):
-    genes.append(gene.get('name'))
-
-
-locus  = {}
-for species in root.findall('.//celldesigner:speciesAlias',ns):
-    species_id = species.get('species')
-
-    coord = species.find('.//celldesigner:bounds',ns)
-    coordVar = {'x' : float(coord.get('x')), 'y':float(coord.get('y'))}
-    locus[species_id] = coordVar
-
-# Extract species (nodes)
-nodes = []
-for species in root.findall('.//species',ns):
-    # print(species)
-    species_id = species.get('id')
-    species_name = species.get('name')
-
-    nodeClass = species.find('.//html:customClass', ns)
-    if nodeClass is not None:
-        classVar = nodeClass.get('type')
+    reactionType = rx.find('.//celldesigner:reactionType', ns).text
+    isReversible = rx.get('reversible')
+    reactInfo = rx.find('.//html:customInfo', ns)
+    if reactInfo is not None:
+        infoVar = reactInfo.get('info')
     else:
-        classVar = 'notAssignedNode'
+        infoVar = 'notAssignedInfo'
 
-    # Extract the reactions catalyzed
-    catalyzed_reactions = []
-    for reaction in species.findall('.//celldesigner:catalyzed', ns):
-        catalyzed_reactions.append(reaction.get('reaction'))
+    reactantList = []
+    productList = []
+    geneList = []
+    geneModifierType = []
+    for reactant in rx.findall('.//celldesigner:baseReactant', ns):
+        source = reactant.get('species')
+        reactantList.append(idToName[source])
+
+    for product in rx.findall('.//celldesigner:baseProduct', ns):
+        target = product.get('species')
+        productList.append(idToName[target])
+
+    for modifier in rx.findall('.//celldesigner:modification', ns):
+        modifier_species = modifier.get('modifiers')
+        modifierType = modifier.get('type')
+        geneList.append(idToName[modifier_species])
+        geneModifierType.append(modifierType)
     
-    # Create a node dictionary
-    node = {
-        'data': {
-            'id': species_id,
-            'label': species_name,
-            'catalyzes': catalyzed_reactions
-        },
-        'classes' : classVar,
-        'position' : locus[species_id]
+    return {
+        'reactionType' : reactionType,
+        'isReversible' : isReversible,
+        'reactInfo' : infoVar,
+        'reactantList' : reactantList,
+        'productList' : productList,
+        'geneList' : geneList,
+        'geneModifierType' : geneModifierType
     }
-    nodes.append(node)
 
 
-# Extract reactions (edges)
-edges = []
-for reaction in root.findall('.//reaction', ns):
-    reaction_id = reaction.get('id')
-    reaction_type = reaction.find('.//celldesigner:reactionType', ns).text
 
-    if reaction_type != 'TRANSCRIPTION':
-        # create a mid node for intersection
-        tempNodeID = f"{reaction_id}_mid"
+def readSbml(filePath,finalNodes,finalNodeSet,finalEdges,reactionNum):
+
+    # Load and parse the XML file
+    tree = ET.parse(filePath)
+    root = tree.getroot()
+
+    genes = []
+    for gene in root.findall('.//celldesigner:gene', ns):
+        genes.append(gene.get('name'))
+
+    nodes = []
+    pathwayName = filePath.split('/')[-1][:-4]
+    idToName = {}
+    for species in root.findall('.//species',ns):
+        # print(species)
+        species_id = species.get('id')
+        species_name = species.get('name')
+        idToName[species_id] = species_name
+
+        if species_name in finalNodeSet:
+            for itr in finalNodes:
+                if itr['data']['label'] == species_name:
+                    itr['data']['pathway'].append(pathwayName)
+                    break
+            continue
+        else:
+            finalNodeSet.add(species_name)
+
+        nodeClass = species.find('.//html:customClass', ns)
+        if nodeClass is not None:
+            classVar = nodeClass.get('type')
+        else:
+            classVar = 'notAssignedNode'
+
+        # Extract the reactions catalyzed
+        catalyzed_reactions = []
+        for reaction in species.findall('.//celldesigner:catalyzed', ns):
+            catalyzed_reactions.append(reaction.get('reaction'))
+        
+        # Create a node dictionary
         node = {
             'data': {
-                'id': tempNodeID,
+                'id': species_name,
+                'label': species_name,
+                'catalyzes': catalyzed_reactions,
+                'pathway' : [pathwayName]
+            },
+            'classes' : classVar
+        }
+        nodes.append(node)
+
+    # Extract reactions (edges)
+    edges = []
+    for reaction in root.findall('.//reaction', ns):
+
+        reactData = parseReaction(reaction, idToName)
+
+        #handle existing case
+        #----------------------------------------------------------------------------------------
+
+        #----------------------------------------------------------------------------------------
+
+        reactionId = reactionNum
+        reactionNum+=1
+
+        #Create Mid node
+        temp = {
+            'data': {
+                'id': f"mid_{reactionId}",
+                'label': f'mid_{reactionId}',
+                'pathway' : [pathwayName]
             },
             'classes' : 'temp'
         }
+        nodes.append(temp)
 
-        isReversible = reaction.get('reversible')
-        reactInfo = species.find('.//html:customInfo', ns)
-        if reactInfo is not None:
-            infoVar = reactInfo.get('info')
-        else:
-            infoVar = 'notAssignedInfo'
-
-        #coord of temp node
-        x = 0
-        y=0
-        totalNodes = 0
-
-        # Reactants to Products
-        for reactant in reaction.findall('.//celldesigner:baseReactant', ns):
-            source = reactant.get('species')
-            
-            x+=locus[source]['x']
-            y+=locus[source]['y']
-            totalNodes+=1
-
+        for reactant in reactData['reactantList']:
             edges.append({
-                'data' : {'source':source, 'target':tempNodeID, 'label':reaction_id,'info':infoVar,'id' : f"{source}_{tempNodeID}"},
+                'data' : {
+                    'source' : reactant,
+                    'target' :f"mid_{reactionId}",
+                },
                 'classes' : 'first_half'
             })
-
-        for product in reaction.findall('.//celldesigner:baseProduct', ns):
-            target = product.get('species')
-
-            x+=locus[target]['x']
-            y+=locus[target]['y']
-            totalNodes+=1
-
+        
+        for reactant in reactData['productList']:
             edges.append({
-                'data' : {'source':tempNodeID, 'target':target, 'label':reaction_id, 'info':infoVar},
+                'data' : {
+                    'source' : f"mid_{reactionId}",
+                    'target' : reactant,
+                },
                 'classes' : 'second_half'
             })
-
-        node['position'] = {'x':x/totalNodes, 'y':y/totalNodes}
-        nodes.append(node)
-
-
-        # Modifiers (e.g., enzymes) connected to reactions
-        for modifier in reaction.findall('.//celldesigner:modification', ns):
-            modifier_species = modifier.get('modifiers')
-            modifierType = modifier.get('type')
+        
+        for gene,modification in zip(reactData['geneList'],reactData['geneModifierType']):
             edges.append({
-                'data': {'source': modifier_species, 'target': tempNodeID, 'label': f"{reaction_id}"},
-                'classes' : modifierType
+                'data' : {
+                    'source' : gene,
+                    'target' : f"mid_{reactionId}",
+                },
+                'classes' : modification
             })
-    else:
-        reactant = reaction.find('.//celldesigner:baseReactant', ns)
-        source = reactant.get('species')
+        
+    finalNodes.extend(nodes)
+    finalEdges.extend(edges)
 
-        product = reaction.find('.//celldesigner:baseProduct', ns)
-        target = product.get('species')
+    return
 
-        edges.append({
-            'data': {'source': source, 'target': target, 'label': f"{reaction_id}"},
-            'classes' : 'TRANSCRIPTION'
-        })
+def readMapping(filePath, finalNodes, finalEdges,finalNodeSet):
+    df = pd.read_csv(filePath)
+
+    transcriptionFactor = df['TF'].unique()
+    targetGene = df['TargetGene'].unique()
+
+    nodes = []
+    edges = []
+    for tf in transcriptionFactor:
+        for tg in targetGene:
+
+            mask = (df['TF'] == tf) & (df['TargetGene'] == tg)
+            temp = df[mask]
+
+            if temp.empty:
+                continue
+            
+            if tf not in finalNodeSet:
+                nodes.append({
+                    'data' : {
+                        'id' : tf,
+                        'label' : tf,
+                    },
+                    'classes' : 'transcriptionFactorGene'
+                })
+            
+            edges.append({
+                'data' : {
+                    'source' : tf,
+                    'target' : tg
+                },
+                'classes' : 'TRANSCRIPTION'
+            })
+    finalNodes.extend(nodes)
+    finalEdges.extend(edges)
+    return nodes,edges
 
 ex_stylesheet = [
     # Style for Gene nodes
@@ -194,6 +254,20 @@ ex_stylesheet = [
         'style': {
             'shape': 'ellipse',
             'background-color': 'pink',
+            'label': 'data(label)',
+            'width': '100px',
+            'height': '40px',
+            'color': '#FFFFFF',
+            'text-valign': 'center',
+            'text-halign': 'center',
+            'font-size' : '10px'
+        }
+    },
+    {
+        'selector': '.lipidSubstrate',
+        'style': {
+            'shape': 'ellipse',
+            'background-color': 'red',
             'label': 'data(label)',
             'width': '100px',
             'height': '40px',
@@ -291,16 +365,63 @@ ex_stylesheet = [
 import dash
 from dash import dcc, html, Input, Output, State
 import dash_cytoscape as cyto
+import json
+import os
 
 app = dash.Dash(__name__)
 
-elements = nodes + edges
+cyto.load_extra_layouts()
+basePath = 'D:/Raylab/LiMeNEx/SBML_Network'
+
+# readSbml('D:/Raylab/LiMeNEx/SBML_Network/networkFile/betaOxidationFattyAcid.xml')
+# readSbml('D:/Raylab/LiMeNEx/SBML_Network/networkFile/fattyAcidSynthesis.xml')
+# readMapping('D:/Raylab/LiMeNEx/SBML_Network/data/fattyAcidSynthesis.csv')
+
+#########################################################################################################
+
+@app.callback(
+    Output('cytoscape', 'elements'),
+    Input('pathwayDropdownOptions', 'value'),
+)
+def handlePathwaySelection(optionList):
+    if optionList is not None:
+        finalNodes = []
+        finalEdges = []
+        finalNodeSet = set()
+        finalReactionList = []
+        reactionNum = 1
+
+        for fileName in optionList:
+            filePath = os.path.join(basePath,'networkFile',f"{fileName}.xml")
+
+            readSbml(filePath=filePath,finalNodes=finalNodes,finalNodeSet=finalNodeSet,finalEdges=finalEdges, reactionNum=reactionNum)
+
+        return finalNodes+finalEdges
+    
+    return []
+
+###########################################################################################################
+with open("D:/Raylab/LiMeNEx/SBML_Network/pathwayDropdownOptions.json", 'r') as file:
+    # Load the JSON data
+    pathwayDropdownOptions = json.load(file)
+
+# elements = finalNodes + finalEdges
 
 app.layout = html.Div([
+    dcc.Dropdown(
+        id = 'pathwayDropdownOptions',
+        options=[
+            {'label' : 'All Pathways', 'value' : 'ALL'},
+            *[{'label': key, 'value': pathwayDropdownOptions[key]} for key in pathwayDropdownOptions.keys()]
+        ],
+        multi= True,
+        maxHeight=300,
+        optionHeight=30
+    ),
     cyto.Cytoscape(
         id='cytoscape',
-        elements=elements,
-        layout={'name': 'preset'},
+        elements=[],
+        layout={'name': 'cose-bilkent'},  #dagre, cose-bilkent, 
         style={'width': '100%', 'height': '600px'},
         boxSelectionEnabled = True,
         stylesheet=ex_stylesheet
@@ -328,7 +449,7 @@ app.layout = html.Div([
 )
 def display_hover(data,style):
     if data:
-        print(data)
+        print(json.dumps(data,indent=2))
         style['display'] = 'none'
         return style, f"source: {data['source']} to target: {data['target']}"
     else:
